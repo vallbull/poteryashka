4f0236fdced3a82fad484fd50f8a17746efb3180
Valeria Bulanova <bulanovavv@yandex-team.ru>
2023-11-15 18:35:34 +0300

    Add app cors and move docker-compose.yml to root

--- poteryashka_service/main.py
+++ poteryashka_service/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 import models
@@ -9,6 +10,13 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = SessionLocal()
