7d06c865c80572d844ad361a47e9955804d9d6f5
Damir Petrov <damir.petrov@yadro.com>
2023-11-17 16:25:57 +0300

    Add register and get_pet pages

--- poteryashka-web/src/GetPet.js
+++ poteryashka-web/src/GetPet.js
@@ -1,30 +1,87 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 
+
+const queryParams = new URLSearchParams(window.location.search);
 
 class GetPetComponent extends Component {
   constructor() {
     super();
+    this.state = {
+      ownerName: "",
+      petName: "",
+      phone: "",
+      address: "",
+    };
   }
 
   async componentDidMount() {
-    const response = await fetch("http://localhost:8080/get_pet?id=1234", {method: "POST"});
-    const json = await response.json();
-    if (true) {
+    const id = queryParams.get('id');
+    const response = await fetch(`http://localhost:8080/get_pet?id=${id}`, {method: "POST"});
+    if (response.status != 200) {
+      window.location.replace(`/register?id=${id}`)
+    } else {
+      const json = await response.json();
       console.log(json)
-      window.location.replace("/register")
-    } else {}
+      this.setState({
+        ownerName: json.owner_name,
+        petName: json.pet_name,
+        phone: json.phone,
+        address: json.address,
+      });
+    }
   }
 
   render() {
     return (
-      <div>
-      </div>
+      <Container>
+        <CardBody> <CardTitle> <h4> Питомец найден! </h4> </CardTitle> </CardBody>
+        <Row xs="5">
+          <Col className="bg-light">
+            Хозяин
+          </Col>
+            {this.state.ownerName}
+          <Col className="bg-light">
+          </Col>
+        </Row>
+        <Row xs="5">
+          <Col className="bg-light">
+            Кличка
+          </Col>
+          <Col className="bg-light">
+            {this.state.petName}
+          </Col>
+        </Row>
+        <Row xs="5">
+          <Col className="bg-light">
+            Телефон 
+          </Col>
+          <Col className="bg-light">
+            {this.state.phone}
+          </Col>
+        </Row>
+        <Row xs="5">
+          <Col className="bg-light">
+            Адрес
+          </Col>
+          <Col className="bg-light">
+            {this.state.address}
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
 
 export default function GetPet() {
-	return (<h1> <GetPetComponent /> </h1>);
+	return (<GetPetComponent />);
 }
 
