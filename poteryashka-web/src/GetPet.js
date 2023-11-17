cf5dea034df681bfb9c34eabce9ed4f8b14c168b
Damir Petrov <damir.petrov@yadro.com>
2023-11-17 11:58:41 +0300

    Add redirection from get_pet handle

--- poteryashka-web/src/GetPet.js
+++ poteryashka-web/src/GetPet.js
@@ -1,4 +1,30 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+
+class GetPetComponent extends Component {
+  constructor() {
+    super();
+  }
+
+  async componentDidMount() {
+    const response = await fetch("http://localhost:8080/get_pet?id=1234", {method: "POST"});
+    const json = await response.json();
+    if (true) {
+      console.log(json)
+      window.location.replace("/register")
+    } else {}
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
 export default function GetPet() {
-	return (<h1> Get pet </h1>);
+	return (<h1> <GetPetComponent /> </h1>);
 }
 
--- poteryashka-web/src/Register.js
+++ poteryashka-web/src/Register.js
@@ -1,4 +1,4 @@
 export default function Register() {
-	return (<h1> Register </h1>);
+	return (<h1> Register your pet </h1>);
 }
 
